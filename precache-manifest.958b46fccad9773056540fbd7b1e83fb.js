9b78a97b9cec30885af5325b2c700308bd189a1a
vikash990 <vikashadubey>
2020-04-11 01:05:25 +0530

    Updates

---
+++ precache-manifest.958b46fccad9773056540fbd7b1e83fb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9923f38c461e9dbff180ce289d59acbc",
+    "url": "/shaurya/index.html"
+  },
+  {
+    "revision": "cc15517927ec8c718ef8",
+    "url": "/shaurya/static/css/main.588fa210.chunk.css"
+  },
+  {
+    "revision": "96c485de24d0f84b9522",
+    "url": "/shaurya/static/js/2.b70f68aa.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/shaurya/static/js/2.b70f68aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc15517927ec8c718ef8",
+    "url": "/shaurya/static/js/main.8889de04.chunk.js"
+  },
+  {
+    "revision": "d7f8085ca9d714e73e12",
+    "url": "/shaurya/static/js/runtime-main.323673c2.js"
+  }
+]);
